45991aedbaa366d5ef4a1678802e6a4e06eeef03
Renato Benatti <renato.benatti7@gmail.com>
2019-01-16 21:12:02 -0200

    Launch server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import app from './app'
- 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('🚀 Listening on port 5000')
-    
-})
+import db from './models'
+const ForestAdmin = require('forest-express-sequelize');
+import * as Secret from './config/secret'
+
+app.use(
+    ForestAdmin.init({
+      modelsDir: __dirname + '/models',
+      envSecret: process.env.FOREST_ENV_SECRET || Secret.default.FOREST_ENV_SECRET,
+      authSecret: process.env.FOREST_AUTH_SECRET || Secret.default.FOREST_AUTH_SECRET,
+      sequelize: db.sequelize
+    })
+  );
+const Sync = async () => {
+    await db.sequelize.sync()
+    app.listen(process.env.PORT || 5000, () => {
+        console.log('🚀 Listening on port 5000')     
+    })
+}
+Sync()
+
+
+//db.sequelize.sync()
+    //.then( () => {
+       // app.listen(process.env.PORT || 5000, () => {
+     //       console.log('🚀 Listening on port 5000')     
+   //     })
+ //   })
