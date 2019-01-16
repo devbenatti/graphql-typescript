6dd8b15f03172e63f389b44f3103f3924b0f1d91
Renato Benatti <renato.benatti7@gmail.com>
2019-01-16 19:34:16 -0200

    Index Model add

---
+++ src/models/index.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as Sequelize from 'sequelize'
+import { DbConnection } from '../interfaces/DbConnectionInterface';
+
+const basename: string = path.basename(module.filename)
+const env: string = process.env.NODE_ENV || 'development';
+const config = require(path.resolve(`${__dirname}./../config/config.json`))[env]
+let db = null
+let sequelize: Sequelize.Sequelize;
+if(!db) {
+    db = {};
+    if (config.use_env_variable) {
+        sequelize = new Sequelize(process.env[config.use_env_variable], config);
+      } else {
+        sequelize = new Sequelize(
+            config.database,
+            config.username,
+            config.password,
+            config
+        )
+      }
+
+    fs.readdirSync(__dirname)
+        .filter(file => {
+            return (
+            file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+            );
+        })
+        .forEach(file => {
+            const model = sequelize['import'](path.join(__dirname, file));
+            db[model['name']] = model;
+        });
+
+    Object.keys(db).forEach(modelName => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+    db['sequelize'] = sequelize
+}
+
+
+export default <DbConnection> db
+
