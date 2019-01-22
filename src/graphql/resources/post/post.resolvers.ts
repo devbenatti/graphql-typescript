1264c9a24dd043f42acb20e04448a5ec6fd44bd0
Renato Benatti <renato.benatti7@gmail.com>
2019-01-21 22:47:46 -0200

    Post Resolver

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,4 +1,4 @@
-import { GraphQLResolveInfo } from "graphql";
+import { GraphQLResolveInfo, formatError } from "graphql";
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { Transaction } from "sequelize";
 
@@ -12,7 +12,7 @@ export const postResolvers = {
         },
         //@ts-ignore
         comments: async(post, {first = 10, offset = 0}, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
-            return await db.Coment
+            return await db.Comment
                 .findAll({
                     where: {post: post.get('id')},
                     limit: first,
@@ -31,6 +31,7 @@ export const postResolvers = {
         },
         //@ts-ignore
         post: async (parent, {id}, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
+            id = parseInt(id)
             const post = await db.Post.findById(id)
             if(!post) throw new Error(`Post with id ${id} not found!`)
             return post;
@@ -46,12 +47,20 @@ export const postResolvers = {
         },
         //@ts-ignore
         updatePost: async (parent, {id, input}, {db}: {db: DbConnection} , info: GraphQLResolveInfo) => {
-            id = parseInt(id)
-            return await db.sequelize.transaction(async (t: Transaction) => {
-                const post = await db.Post.findById(id)
-                if(!post) throw new Error (`Post with id ${id} not found!`) 
-                return await db.Post.update(input,{where: {id}, transaction: t})
-            })
+            try {
+                id = parseInt(id)
+                const response = await db.sequelize.transaction(async (t: Transaction)=> {
+                    const user = await db.Post.findById(id)
+                    if(!user) {
+                        throw new Error (`User with id ${id} not found!`)
+                    }// retorna o usuário atualizado
+                    await db.Post.update(input, {where: {id}})
+                    return await db.Post.findById(id, {transaction: t }) 
+                })
+                return response
+            } catch (error) {
+                return formatError(error)
+            }
         },
         //@ts-ignore
         deletePost: async (parent, {id}, {db}: {db: DbConnection} , info: GraphQLResolveInfo) => {
@@ -64,5 +73,5 @@ export const postResolvers = {
             })
         },
     }
-
 }
+
