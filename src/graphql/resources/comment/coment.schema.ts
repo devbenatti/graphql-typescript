df0f6985f8ed5b8ab28262c4fae6403321438d63
Renato Benatti <renato.benatti7@gmail.com>
2019-01-21 22:47:58 -0200

    Comment Schema

--- src/graphql/resources/comment/coment.schema.ts
+++ src/graphql/resources/comment/coment.schema.ts
@@ -14,7 +14,7 @@ const commentTypes = `
     }    
 `
 const commentQueries = `
-    commentByPost(post: ID!, first: Int, offset: Int): [ Comment! ]! 
+    commentByPost(postId: ID!, first: Int, offset: Int): [ Comment! ]! 
 `
 const commentMutations = `
     createComment(input: CommentInput!): Comment
