93c0325ee6330e2527cc9c74ae661fef47388b66
Renato Benatti <renato.benatti7@gmail.com>
2019-01-21 22:46:22 -0200

    Comment Model

--- src/models/ComentModel.ts
+++ src/models/ComentModel.ts
@@ -13,14 +13,13 @@ export interface ComentAttributes {
 export interface ComentInstance extends Sequelize.Instance<ComentAttributes>, ComentAttributes {
 
 }
-
 export interface ComentModel extends  Sequelize.Model<ComentInstance, ComentAttributes> {
     prototype?;
     associate?(models: ModelsInterface): void
 }
 
 export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes):ComentModel => {
-    const Coment: ComentModel = sequelize.define<ComentInstance,ComentAttributes>('Comment', {
+    const Comentario: ComentModel = sequelize.define<ComentInstance,ComentAttributes>('Comment', {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -35,15 +34,15 @@ export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes):
         tableName: 'comments'
     })
 
-    Coment.associate = (models: ModelsInterface):void => {
-        Coment.belongsTo(models.User, {
+    Comentario.associate = (models: ModelsInterface):void => {
+        Comentario.belongsTo(models.User, {
             foreignKey: {
                 allowNull: false,
                 field: 'user',
                 name: 'user'
             }
         })
-        Coment.belongsTo(models.Post, {
+        Comentario.belongsTo(models.Post, {
             foreignKey: {
                 allowNull: false,
                 field: 'post',
@@ -53,5 +52,5 @@ export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes):
     }
 
     
-    return Coment
+    return Comentario
 }
