aff40ecff22f6d770a995d27d9bf059c071e8a7f
Renato Benatti <renato.benatti7@gmail.com>
2019-02-10 13:50:39 -0200

    Post Resolver

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,77 +1,101 @@
 import { GraphQLResolveInfo, formatError } from "graphql";
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { Transaction } from "sequelize";
-
+import { compose } from "../../composable/composable.resolver";
+import { AuthUser } from "../../../interfaces/AuthUserInterface";
+import {throwError} from '../../../utils/utils'
+import {authResolvers} from '../../composable/auth.resolver'
+import { DataLoaders } from "../../../interfaces/DataLoadersInterace";
+import { RequestedFields } from "../../ast/RequestedFields";
 export const postResolvers = {
 
     Post: {
         //@ts-ignore
-        author: async (post, args, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
-            return await db.User
-                .findById(post.get('author'))
+        author: async (post, args, {db, dataloaders: {userLoader}}:{db: DbConnection, dataloaders  : DataLoaders}, info:  GraphQLResolveInfo) => {
+            /*return await db.User
+                .findById(post.get('author'))*/
+            return userLoader
+                .load(post.get('author'))
         },
         //@ts-ignore
-        comments: async(post, {first = 10, offset = 0}, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
-            return await db.Comment
+        comments: async(post, {first = 10, offset = 0}, context, info:  GraphQLResolveInfo) => {
+            const attributes = context.requestedFields.getFields(info)
+            return await context.db.Comment
                 .findAll({
                     where: {post: post.get('id')},
                     limit: first,
-                    offset
+                    offset,
+                    attributes
                 })
         },
     },
     Query: {
         //@ts-ignore
-        posts: async (parent, {first = 10, offset = 0}, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
+        posts: async (parent, {first = 10, offset = 0}, {db, requestedFields}:{db: DbConnection, requestedFields: RequestedFields}, info:  GraphQLResolveInfo) => {
+            const attributes = requestedFields.getFields(info, {
+                keep: ['id'],
+                exclude: ['comments']
+            })
             return await db.Post
                 .findAll({
                     limit: first,
-                    offset
+                    offset,
+                    attributes
                 })
-        },
+        }, 
         //@ts-ignore
-        post: async (parent, {id}, {db}:{db: DbConnection}, info:  GraphQLResolveInfo) => {
+        post: async (parent, {id}, {db, requestedFields}:{db: DbConnection, requestedFields: RequestedFields}, info:  GraphQLResolveInfo) => {
             id = parseInt(id)
-            const post = await db.Post.findById(id)
-            if(!post) throw new Error(`Post with id ${id} not found!`)
+            const attributes = requestedFields.getFields(info, {
+                keep: ['id'],
+                exclude: ['comments']
+            })
+            const post = await db.Post
+            .findByPk(id,{
+                attributes
+            })
+            throwError(!post, `Post with id ${id} not found!`)
             return post;
         },
     },
     Mutation: {
         //@ts-ignore
-        createPost : async (parent, {input}, {db}: {db: DbConnection} , info: GraphQLResolveInfo) => {
+        createPost : compose(... authResolvers)(async (parent, {input}, {db, authUser}: {db: DbConnection, authUser: AuthUser} , info: GraphQLResolveInfo) => {
+            input.author = authUser.id
             return db.sequelize.transaction(async (t: Transaction) => {
                 return await db.Post.create(input, {transaction: t})
             })
             
-        },
+        }),
         //@ts-ignore
-        updatePost: async (parent, {id, input}, {db}: {db: DbConnection} , info: GraphQLResolveInfo) => {
-            try {
-                id = parseInt(id)
-                const response = await db.sequelize.transaction(async (t: Transaction)=> {
-                    const user = await db.Post.findById(id)
-                    if(!user) {
-                        throw new Error (`User with id ${id} not found!`)
-                    }// retorna o usuário atualizado
-                    await db.Post.update(input, {where: {id}})
-                    return await db.Post.findById(id, {transaction: t }) 
-                })
-                return response
-            } catch (error) {
-                return formatError(error)
-            }
-        },
+        updatePost: compose(... authResolvers)(async (parent, {id, input}, {db, authUser, requestedFields}: {db: DbConnection, authUser: AuthUser, requestedFields: RequestedFields} , info: GraphQLResolveInfo) => {
+            const attributes = requestedFields.getFields(info, {
+                keep: ['id'],
+                exclude: ['comments']
+            })
+            id = parseInt(id)
+            const response = await db.sequelize.transaction(async (t: Transaction)=> {
+                const post = await db.Post.findByPk(id)
+                throwError(!post, `Post with id ${id} not found!`)
+                throwError(post.get('author') != authUser.id, `Unauthorized! You can only edit posts by yourself`)
+                input.author = authUser.id
+                await db.Post.update(input, {where: {id}})
+                return await db.Post.findByPk(id, {transaction: t , attributes}) 
+            })
+            return response
+    
+        }),
         //@ts-ignore
-        deletePost: async (parent, {id}, {db}: {db: DbConnection} , info: GraphQLResolveInfo) => {
+        deletePost: compose(... authResolvers)(async (parent, {id}, {db,authUser}: {db: DbConnection, authUser: AuthUser} , info: GraphQLResolveInfo) => {
             id = parseInt(id)
             return db.sequelize.transaction(async(t: Transaction) => {
-                const post = await db.Post.findById(id)
-                if(!post) throw new Error(`Post with id ${id} not found!`);
+                const post = await db.Post.findByPk(id)
+                throwError(!post, `Post with id ${id} not found!`)
+                throwError(post.get('author') != authUser.id, `Unauthorized! You can only delete posts by yourself`)
                 const postDeleted = await db.Post.destroy({where: {id}, transaction: t})
                 return !!postDeleted
             })
-        },
+        }),
     }
 }
 
